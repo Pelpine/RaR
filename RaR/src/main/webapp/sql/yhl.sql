7154db73d9b37f90e4ba442006f8e6885f4a30cb
user <user@DESKTOP-55FTKB2>
2024-05-23 17:31:55 +0900

    테이블 생성(주문정보,상품,장바구니,주문별 구매 상품)

--- RaR/src/main/webapp/sql/yhl.sql
+++ RaR/src/main/webapp/sql/yhl.sql
@@ -1,65 +1,59 @@
---장바구니(CART)
-CREATE TABLE "CART" (
-	"order_item"	NUMBER		NOT NULL,
-	"user_num"	NUMBER		NOT NULL,
-	"item_num"	NUMBER		NOT NULL
+--RAR_ORDER(주문 정보 테이블)
+CREATE TABLE RAR_ORDER (
+	order_num	NUMBER		NOT NULL,
+	total_amount	NUMBER		NOT NULL,
+	order_date	DATE	DEFAULT SYSDATE	NOT NULL,
+	order_name	VARCHAR2(200)		NOT NULL,
+	zipcode	NUMBER		NOT NULL,
+	address1	VARCHAR2(200)		NOT NULL,
+	address2	VARCHAR2(200)		NOT NULL,
+	order_pay	NUMBER		NOT NULL,
+	status	NUMBER	DEFAULT 1	NOT NULL,
+	user_num	NUMBER		NOT NULL,
+	bk_num	NUMBER		NOT NULL,
+	constraint RAR_ORDER_pk primary key (order_num),
+	constraint RAR_ORDER_user_fk foreign key (user_num) references MEMBER (user_num),
+	constraint RAR_ORDER_book_fk foreign key (bk_num) references BOOK (bk_num)
 );
-
-COMMENT ON COLUMN "CART"."user_num" IS '시퀀스를 이용해 값 지정';
-
-ALTER TABLE "CART" ADD CONSTRAINT "PK_CART" PRIMARY KEY (
-	"order_item"
-);
-
+CREATE sequence RAR_ORDER_SEQ;
 
 --ITEM(상품 테이블)
-CREATE TABLE "item" (
-	"item_num"	NUMBER		NOT NULL,
-	"item_price"	NUMBER		NOT NULL,
-	"item_grade"	NUMBER		NOT NULL,
-	"item_img"	VARCHAR2(200)		NOT NULL,
-	"item_inf"	CLOB		NOT NULL,
-	"bk_num"	NUMBER		NOT NULL,
-	"approval_id"	NUMBER		NOT NULL
+CREATE TABLE ITEM (
+	item_num	NUMBER		NOT NULL,
+	item_price	NUMBER		NOT NULL,
+	item_grade	NUMBER		NOT NULL,
+	item_img	VARCHAR2(200)		NOT NULL,
+	item_info	CLOB		NOT NULL,
+	bk_num	NUMBER		NOT NULL,
+	approval_id	NUMBER		NOT NULL,
+	constraint ITEM_pk primary key (item_num),
+	constraint ITEM_book_fk foreign key (bk_num) references BOOK (bk_num),
+	constraint ITEM_approval_fk foreign key (approval_id) references BOOK_APPROVAL (approval_id)
+);
+CREATE sequence ITEM_SEQ;
+
+--RAR_ORDER_ITEM(주문별 구매 상품)
+CREATE TABLE RAR_ORDER_ITEM (
+	order_num	NUMBER		NOT NULL,
+	item_num	NUMBER		NOT NULL,
+	bk_num		NUMBER		NOT NULL,
+	constraint RAR_ORDER_ITEM_order_fk foreign key (order_num) references RAR_ORDER (order_num),
+	constraint RAR_ORDER_ITEM_item_fk foreign key (item_num) references ITEM (item_num),
+	constraint RAR_ORDER_ITEM_bk_fk foreign key (bk_num) references BOOK (bk_num)
 );
 
-COMMENT ON COLUMN "item"."item_grade" IS '도서 상태(1:매우 양호2:양호3:불량)';
-
-ALTER TABLE "item" ADD CONSTRAINT "PK_ITEM" PRIMARY KEY (
-	"item_num"
+--CART(장바구니)
+CREATE TABLE CART (
+	user_num	NUMBER		NOT NULL,
+	item_num	NUMBER		NOT NULL,
+	bk_num		NUMBER		NOT NULL,
+	constraint CART_user_fk foreign key (user_num) references MEMBER(user_num),
+	constraint CART_item_fk foreign key (item_num) references ITEM (item_num),
+	constraint CART_bk_fk foreign key (bk_num) references BOOK (bk_num)
 );
 
 
---ORDER_ITEM
-CREATE TABLE "ORDER_ITEM" (
-	"order_num"	NUMBER		NOT NULL,
-	"item_num"	NUMBER		NOT NULL
-);
 
-COMMENT ON COLUMN "ORDER_ITEM"."order_num" IS '주문내역 고유번호, sequence 생성';
 
-ALTER TABLE "CART" ADD CONSTRAINT "PK_CART" PRIMARY KEY (
-	"order_item"
-);
 
---ORDER
-CREATE TABLE "ORDER" (
-	"order_num"	NUMBER		NOT NULL,
-	"total_amount"	NUMBER		NOT NULL,
-	"order_date"	DATE	DEFAULT SYSDATE	NOT NULL,
-	"order_name"	VARCHAR2(200)		NOT NULL,
-	"zipcode"	NUMBER		NOT NULL,
-	"address1"	VARCHAR2(200)		NOT NULL,
-	"address2"	VARCHAR2(200)		NOT NULL,
-	"order_pay"	NUMBER		NOT NULL,
-	"status"	NUMBER	DEFAULT 1	NOT NULL,
-	"user_num"	NUMBER		NOT NULL
-);
 
-COMMENT ON COLUMN "ORDER"."order_num" IS '주문내역 고유번호, sequence 생성';
-
-COMMENT ON COLUMN "ORDER"."user_num" IS '시퀀스를 이용해 값 지정';
-
-ALTER TABLE "ORDER" ADD CONSTRAINT "PK_ORDER" PRIMARY KEY (
-	"order_num"
-);
