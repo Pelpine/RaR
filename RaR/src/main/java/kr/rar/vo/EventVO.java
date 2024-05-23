ca05d971c36c2a36d1539edb55d48b078ddc1bec
user <user@DESKTOP-IBP4612>
2024-05-23 17:08:31 +0900

    vo 생성

--- RaR/src/main/java/kr/rar/vo/EventVO.java
+++ RaR/src/main/java/kr/rar/vo/EventVO.java
@@ -1,5 +1,62 @@
 package kr.rar.vo;
 
-public class EventVO {
+import java.sql.Date;
 
+public class EventVO {
+	private int event_num;
+	private String name;
+	private Date reg_date;
+	private String filename;
+	private String content;
+	private Date start_date;
+	private Date end_date;
+	private int user_num;
+	public int getEvent_num() {
+		return event_num;
+	}
+	public void setEvent_num(int event_num) {
+		this.event_num = event_num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	public int getUser_num() {
+		return user_num;
+	}
+	public void setUser_num(int user_num) {
+		this.user_num = user_num;
+	}
 }
