b1a8bae1d4442018b39efbb274f75a46804318f0
user <user@DESKTOP-IBP4612>
2024-05-23 14:34:59 +0900

    table

--- RaR/src/main/webapp/sql/khc.sql
+++ RaR/src/main/webapp/sql/khc.sql
@@ -1,1 +1,12 @@
-sddscd
+CREATE TABLE EVENT_LIST(
+	event_num number primary key not null,
+	event_name varchar2(300) not null,
+	user_num number not null,
+	event_date date default sysdate not null,
+	event_img varchar2(500),
+	event_content varchar2(300) not null,
+	event_start date not null,
+	event_end date not null,
+	constraint event_list_fk foreign key (user_num) 
+                                references user (user_num)
+);
