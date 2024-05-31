48bad4e643cc2869c0c0737e4ba5d04f6ac321e1
user <user@DESKTOP-IBP4612>
2024-05-31 14:39:47 +0900

    이벤트 보상 vo 추가

---
+++ RaR/src/main/java/kr/rar/vo/EventRewardVO.java
@@ -0,0 +1,35 @@
+package kr.rar.vo;
+
+public class EventRewardVO {
+	private int event_num;
+	private int user_num;
+	private int reward_num;
+	private int point;
+	public int getEvent_num() {
+		return event_num;
+	}
+	public void setEvent_num(int event_num) {
+		this.event_num = event_num;
+	}
+	public int getUser_num() {
+		return user_num;
+	}
+	public void setUser_num(int user_num) {
+		this.user_num = user_num;
+	}
+	public int getReward_num() {
+		return reward_num;
+	}
+	public void setReward_num(int reward_num) {
+		this.reward_num = reward_num;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	
+
+}
