c3b8580c6ae070904ac980773ef1970a085de411
user <user@DESKTOP-IBP4612>
2024-05-31 15:46:09 +0900

    리워드 지급 날짜 저장

--- RaR/src/main/java/kr/rar/vo/EventRewardVO.java
+++ RaR/src/main/java/kr/rar/vo/EventRewardVO.java
@@ -1,10 +1,13 @@
 package kr.rar.vo;
 
+import java.sql.Date;
+
 public class EventRewardVO {
 	private int event_num;
 	private int user_num;
 	private int reward_num;
 	private int point;
+	private Date provide_date;
 	public int getEvent_num() {
 		return event_num;
 	}
@@ -29,6 +32,14 @@ public class EventRewardVO {
 	public void setPoint(int point) {
 		this.point = point;
 	}
+	public Date getProvide_date() {
+		return provide_date;
+	}
+	public void setProvide_date(Date provide_date) {
+		this.provide_date = provide_date;
+	}
+	
+	
 	
 	
 
