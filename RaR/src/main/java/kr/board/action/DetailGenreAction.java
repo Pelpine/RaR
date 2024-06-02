27259995399fca571a02626d8783c103d8d14a8e
tlsdu <tlsdu@192.168.219.196>
2024-06-02 17:36:27 +0900

    장르상세

---
+++ RaR/src/main/java/kr/board/action/DetailGenreAction.java
@@ -0,0 +1,30 @@
+package kr.board.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.rar.dao.BoardDAO;
+import kr.rar.vo.GenreVO;
+import kr.controller.Action;
+import kr.util.StringUtil;
+
+public class DetailGenreAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//글번호 반환
+				int bg_num = Integer.parseInt(request.getParameter("board_num"));
+				
+				BoardDAO dao = BoardDAO.getInstance();
+				
+				
+				GenreVO genre = dao.getGenre(bg_num);
+				//HTML을 사용하지 않음
+				genre.setBg_title(StringUtil.useNoHTML(genre.getBg_title()));
+				
+				request.setAttribute("genre", genre);
+				
+				return "/WEB-INF/views/board/detailGenre.jsp";
+	}
+
+}
