fa1cf949aea0ee8aca1876e51e713e65eccf50d9
김한철 <@DESKTOP-J246VOT>
2024-06-02 15:50:19 +0900

    출석체크 aJax 방식으로 변경

--- RaR/src/main/java/kr/event/action/AttendanceEventAction.java
+++ RaR/src/main/java/kr/event/action/AttendanceEventAction.java
@@ -1,8 +1,13 @@
 package kr.event.action;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 import kr.controller.Action;
 import kr.rar.dao.EventDAO;
@@ -11,11 +16,13 @@ public class AttendanceEventAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		Map<String,String> mapAjax = 
+		          new HashMap<String,String>();
 		HttpSession session = request.getSession();
 		Integer user_num = (Integer)session.getAttribute("user_num");
 		
 		if(user_num == null) {
-			return "redirect:/member/loginForm.do";
+			mapAjax.put("result", "logout");
 		}
 		boolean checkat = false;
 		EventDAO dao = EventDAO.getInstance();
@@ -24,17 +31,16 @@ public class AttendanceEventAction implements Action{
 			dao.attendance(user_num);
 			int point = 50;
 			int event_num = 100;
-			dao.updatePoint(user_num , point,event_num);			
-			request.setAttribute("notice_msg", "출석체크 완료! 50point 지급");
-			request.setAttribute("notice_url", request.getContextPath()+"/member/myPage.do");		
+			dao.updatePoint(user_num , point,event_num);
+			mapAjax.put("result", "success");		
 		}else if(checkat) {
-			request.setAttribute("notice_msg", "오늘은 이미 출석 체크 완료!");
-			request.setAttribute("notice_url", request.getContextPath()+"/member/myPage.do");
-				
+			mapAjax.put("result", "alreadyAttendance");
 		}
-		return "/WEB-INF/views/common/alert_view.jsp";
+		ObjectMapper mapper = new ObjectMapper();
+		String ajaxData = mapper.writeValueAsString(mapAjax);
+		
+		request.setAttribute("ajaxData", ajaxData);
+		
+		return "/WEB-INF/views/common/ajax_view.jsp";
 		}
-		
-		
-
 }
---
+++ RaR/src/main/webapp/js/attendance.js
@@ -0,0 +1,23 @@
+$(function(){	
+	$('#attendance_button').click(function(){
+	    $.ajax({
+	        url:'../event/attendanceEvent.do',
+	        type:'post',
+	        dataType:'json',
+	        success: function(param) {
+	           if(param.result == 'logout'){
+					alert('로그인 후 사용하세요');
+				}else if(param.result == 'alreadyAttendance'){
+					alert('오늘은 이미 출석체크 완료!');
+				}else if(param.result == 'success'){
+					alert('출석체크 완료. 50point 지급!');
+				}else{
+					alert('출석체크 오류 발생');
+				}
+	        },
+	        error: function() {
+	            alert('네트워크 오류 발생');
+	        }
+	    });
+	});
+});
